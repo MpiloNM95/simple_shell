041c97c2ac4fd6e69306c566c8e7e141330c8045
BongsMona <bongisisa.mona@gmail.com>
2022-09-07 01:41:13 -0700

    Team Task

--- otherhelpers.c
+++ otherhelpers.c
@@ -47,3 +47,30 @@ void _puts(char *str)
 		 _putchar(str[c]);
 	_putchar('\n');
 }
+
+/**
+ * _putchar - prints a character
+ * @c: character to print
+ *
+ * Return: return value of write syscall
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: character array to fill
+ * @b: constant byte to fill with
+ * @n: how many bytes to fill
+ * Return: the pointer to the char array
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}

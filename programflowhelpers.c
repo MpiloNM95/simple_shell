83ef0f5623129d961bcf03ed983bf0f77dbb62f3
MpiloNM95 <mpilonmafu95@gmail.com>
2022-09-07 02:18:20 -0700

    Team Task

--- programflowhelpers.c
+++ programflowhelpers.c
@@ -101,3 +101,79 @@ char *_fullpathbuffer(char **av, char *PATH, char *copy)
 	free(copy);
 	return (fullpathbuffer);
 }
+
+/**
+ * checkbuiltins - check if first user string is a built-in
+ * @av: pointer to array of user of strings
+ * @buffer: pointer to user string
+ * @exitstatus: exit status of execve
+ * Return: 1 if user string is equal to env or 0 otherwise
+ */
+int checkbuiltins(char **av, char *buffer, int exitstatus)
+{
+	int i;
+
+	if (_strcmp(av[0], "env") == 0)
+	{
+		_env();
+		for (i = 0; av[i]; i++)
+			free(av[i]);
+		free(av);
+		free(buffer);
+		return (1);
+	}
+	else if (_strcmp(av[0], "exit") == 0)
+	{
+		for (i = 0; av[i]; i++)
+			free(av[i]);
+		free(av);
+		free(buffer);
+		exit(exitstatus);
+	}
+	else 
+		return (0);
+}
+
+/**
+ * _forkprocess - create child process to execute based on user input
+ * @av: pointer to array of user of strings
+ * @buffer: pointer to user string
+ * @fullpathbuffer: pointer to user input
+ *
+ * Return: 0 on success
+ */
+int _forkprocess(char **av, char *buffer, char *fullpathbuffer)
+{
+	int i, status, result, exitstatus = 0;
+	pid_t pid;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("Error");
+		exit(1);
+	}
+	if (pid == 0)
+	{
+		result = execve(fullpathbuffer, av, environ);
+		if (result == -1)
+		{
+			perror(av[0]);
+			for (i = 0; av[i]; i++)
+				free(av[i]);
+			free(av);
+			free(buffer);
+			exit(127);
+		}
+	}
+	wait(&status);
+	if (WIFEXITED(status))
+	{
+		exitstatus = WEXITSTATUS(status);
+	}
+	for (i = 0; av[i]; i++)
+		free(av[i]);
+	free(av);
+	free(buffer);
+	return (exitstatus);
+}

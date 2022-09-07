dbc7938278b94aa7bce43cc9029c648166e88dda
BongsMona <bongisisa.mona@gmail.com>
2022-09-07 02:47:13 -0700

    Team Task

--- stringhelpers.c
+++ stringhelpers.c
@@ -69,3 +69,39 @@ int _strcmp(const char *s1, const char *s2)
 	else
 		return (*s1 - *s2);
 }
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: char type
+ * @src: char type
+ * i - integer type
+ * j - integer type
+ * Return: char type
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0'; j++, i++)
+		dest[i] = src[j];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: character type
+ * i - integer type
+ * Return: integer type
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}

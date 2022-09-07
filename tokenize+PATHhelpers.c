1d794351921672ac15bd0e00c01401326985933a
MpiloNM95 <mpilonmafu95@gmail.com>
2022-09-07 03:18:12 -0700

    Team Task

--- tokenize+PATHhelpers.c
+++ tokenize+PATHhelpers.c
@@ -56,3 +56,43 @@ int _splitPATH(char *str)
 	}
 	return (wordcount);
 }
+
+/**
+ * _PATHstrcmp - compares PATH with environ to find PATH value
+ * @s1: pointer PATH string
+ * @s2: pointer to environ string with actual value
+ *
+ * Return: 0 on success
+ */
+int _PATHstrcmp(const char *s1, const char *s2)
+{
+	int i;
+
+	for (i = 0; s2[i] != '='; i++)
+	{
+		if (s1[i] != s2[i])
+			return (-1);
+	}
+	return (0);
+}
+
+/**
+ * _concat - concats user string with PATH member string and /
+ * @tmp: static array to store concatenated string
+ * @av: pointer to array of user strings
+ * @tok: pointer to PATH token
+ *
+ * Return: 0 on success
+ */
+char *_concat(char *tmp, char **av, char *tok)
+{
+	int len = 0;
+
+	_memset(tmp, 0, 256);
+	len = _strlen(tok) + _strlen(av[0]) + 2;
+	_strcat(tmp, tok);
+	_strcat(tmp, "/");
+	_strcat(tmp, av[0]);
+	tmp[len - 1] = '\0';
+	return (tmp);
+}
